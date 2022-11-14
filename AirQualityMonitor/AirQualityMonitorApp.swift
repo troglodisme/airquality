045f754d4b1b54607895f50cf78b641517c462f7
Giulio Ammendola <giulio.ammendola@network.rca.ac.uk>
2022-11-14 22:55:16 +0100

    Initial Commit

---
+++ AirQualityMonitor/AirQualityMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityMonitorApp.swift
+//  AirQualityMonitor
+//
+//  Created by Giulio on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AirQualityMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
